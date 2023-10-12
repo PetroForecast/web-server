82d46b772a9bf5a4b2c70194a2454f1a28289f73
fygure <92011471+fygure@users.noreply.github.com>
2023-10-11 19:03:21 -0500

    users/register POST request route, controller, and model with dummy data complete

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,25 +12,39 @@ async function getAllUsers(req, res) {
 }
 ////////////////////////////////////////////////////////////////////////
 async function loginUser(req, res) {
-    const { username, password } = req.body;
     try {
-        const user = await UserModel.loginUser(username, password);
-
+        const { username, password } = req.body;
+        const user = await UserModel.loginUser(String(username), String(password));
         if (!user) {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
-
         return res.json(user);
-
     } catch (error) {
         console.error('Error logging in:', error);
         return res.status(500).json({ error: 'Error logging in' });
     }
 }
 ////////////////////////////////////////////////////////////////////////
-
+async function registerUser(req, res) {
+    try {
+        const { username, password } = req.body;
+        if (Object.keys(req.body).length === 0 || Object.values(req.body).some(value => value === '')) {
+            return res.status(422).json({ error: 'Empty Request Body or Empty String Values' });
+        }
+        const user = await UserModel.registerUser(String(username), String(password));
+        if (!user) {
+            return res.status(401).json({ error: 'Error Retrieving User' });
+        }
+        return res.json(user);
+    } catch (error) {
+        console.error('Error logging in:', error);
+        return res.status(500).json({ error: 'Error Registering User' });
+    }
+}
+////////////////////////////////////////////////////////////////////////
 module.exports = {
     getAllUsers,
     loginUser,
+    registerUser,
     // Add more controller methods
 };
--- routes/User.js
+++ routes/User.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/UserController');
 
 router.get('/', userController.getAllUsers);
 router.post('/login', userController.loginUser);
-
+router.post('/register', userController.registerUser);
 
 module.exports = router;
